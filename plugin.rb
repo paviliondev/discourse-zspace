7136f780147aa00f1596db9ccfb983de07379446
Angus McLeod <angus@mcleod.org.au>
2019-08-02 10:50:55 +1000

    Namespace changes

--- plugin.rb
+++ plugin.rb
@@ -9,19 +9,19 @@ register_asset 'stylesheets/common/zspace.scss'
 register_svg_icon "download" if respond_to?(:register_svg_icon)
 
 after_initialize do
-  add_to_serializer(:topic_view, :activity_file) {
-    if object.topic.custom_fields['activity_file']
+  add_to_serializer(:topic_view, :zspace_upload) {
+    if object.topic.custom_fields['zspace_upload']
       begin
-        JSON.parse(object.topic.custom_fields['activity_file'])
+        JSON.parse(object.topic.custom_fields['zspace_upload'])
       rescue JSON::ParserError => e
         puts e
       end 
     end
   }
-  add_to_serializer(:topic_list_item, :activity_file) {
-    if object.custom_fields['activity_file']
+  add_to_serializer(:topic_list_item, :zspace_upload) {
+    if object.custom_fields['zspace_upload']
       begin
-        JSON.parse(object.custom_fields['activity_file'])
+        JSON.parse(object.custom_fields['zspace_upload'])
       rescue JSON::ParserError => e
         puts e
       end
